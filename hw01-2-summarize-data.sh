c995db8fc8b447095d001ffa6a404df7fc77fce2
sv642-hub <sv642@georgetown.edu>
2026-09-09 06:26:57 +0000

    part 2 files

---
+++ hw01-2-summarize-data.sh
@@ -0,0 +1,9 @@
+echo "filename,size,num_lines" > wikimedia_data_summary.csv
+
+for file in data/*.csv
+do
+	name=$(basename "$file")
+	size=$(du -h "$file" | cut -f1)
+	lines=$(wc -l < "$file")
+	echo "$name,$size,$lines" >> wikimedia_data_summary.csv
+done
